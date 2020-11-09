e602eef201c60fc25ff7c41fe95d2ce63a751766
Michiel van Breugel <mvbreugel@gmail.com>
2020-11-09 08:33:35 +0800

    adding files

---
+++ R/repeat_simulation.R
@@ -0,0 +1,316 @@
+#' Creates a data-frame with differences between regression slopes of two
+#' levels of a categorical variable, using simulation approach (see below). The
+#' input data needs to have one categorical predictor variable, one continuous
+#' predictor variable and one continuous response variable. The function fits a
+#' linear regression model with lm(). It is your responsibility to explore the
+#' data and check all linear regression assumptions before using  this function.
+#' Any data transformation has to be done before using this function.
+#' The simulation modeling is done as follows:
+#' The continuous predictor values are sampled from the observed range of values,
+#' with sample probabilities weighted using the density curve fitted on the
+#' observed values. The two levels of the categorical predictor values are
+#' sampled with probabilities equal to the relative frequencies of the levels
+#' in the original data. The number of predictor values sampled is equal to the
+#' number data points in the original data. Response variable data is obtained
+#' by adding prediction errors to a slope of zero (type = simulate_0) or to the
+#' predicted y-values (type = simulate_ci). Both the residuals and predicted
+#' values are computed from the regression model that is fitted on the original
+#' data. Output is a dataframe with two columns: replicate and stats. The latter
+#' is the difference between the estimated regression slopes for the two levels
+#' of the categorical predictor variable.
+#' @mydata input data. A data frame with one categorical variable with two levels and two continuous variables
+#' @continuous_variable continuous predictor variable
+#' @categorical_variable categorical predictor variable. Maximum of two levels
+#' @response_variable Numerical response variable
+#' @type You want to test the null model of no difference between the slopes:
+#' type = "null-hypothesis". You want to get the confidence interval around the
+#' observed difference in regression slopes": "confidence-interval"
+#' @reps number of replicates
+#' @sample_size Number of data points generated for the simulated data sets. Defined
+#' by a value that will be multiplied with the sample size of the original data.
+#' Default is 1, which result in the same sample size as original data.
+#' @equal_variance default is "yes". If "no", separate error terms are calculated
+#' for each group
+#' @error_term value is multiplied with the standard deviation of the residuals
+#' of the regression model on the original data. This allows the user to assess
+#' the effect of a lower or higher than noise than observed in the data. Default
+#' value is 1, resulting in a error term equal to the sd of the observed residuals.
+#' @import readr
+#' @import purrr
+#' @import tidyr
+#' @import tidyselect
+#' @import broom
+#' @import rsample
+#' @import dplyr
+#' @export
+simulate_slope_differences <- function(
+  mydata = forest_succession,
+  continuous_variable = "age",
+  categorical_variable = "top_pos",
+  response_variable = "sr",
+  type="null-hypothesis",
+  reps=1000,
+  sample_size = 1,
+  error_term = c(1, 2),
+  equal_variance = TRUE){
+
+
+  mydata <- mydata %>%
+    dplyr::rename(x_cont = tidyselect::all_of(continuous_variable),
+           x_cat = tidyselect::all_of(categorical_variable),
+           y_var = tidyselect::all_of(response_variable)) %>%
+    dplyr::select(x_cont, x_cat, y_var)
+
+    # getting regression statistics of original data
+    # Linear regression model
+    regr_model <- lm(y_var ~ x_cont * x_cat, data = mydata)
+
+    # Density distribution of the continuous x-values
+    dens_distr_x <-density(mydata$x_cont,
+                           n=max(1024,
+                                 round(sample_size*nrow(mydata),0)))
+
+    # Levels of the categorical x-value, with original proportions
+    suppressMessages(
+      cat_var <- mydata %>% group_by(x_cat) %>%
+        dplyr::summarise(nr = n()) %>%
+        ungroup()
+    )
+
+
+  if(equal_variance == TRUE){
+
+    # get standard deviation of the residuals of original regression model
+    suppressMessages(
+      sd_original_residuals <- mydata %>%
+      mutate(predy = predict(regr_model)) %>%
+      dplyr::summarise(residuals = error_term*sd(predy-y_var)) %>%
+      as.numeric()
+    )
+
+      if(type == "confidence-interval"){
+
+        simdat = list()
+
+        for(i in 1:reps){
+          # sampling data (predicted values + sampling error)
+          simdat[[i]] <-
+            data.frame(x_cont = sample(dens_distr_x[[1]],
+                                       round(sample_size*nrow(mydata),0),
+                                       prob = dens_distr_x[[2]],
+                                       replace=F),
+                       x_cat = sample(cat_var$x_cat,
+                                      size = round(sample_size*nrow(mydata),0),
+                                      prob = cat_var$nr,
+                                      replace = T)) %>%
+            mutate(y_pop = predict(regr_model, newdata = .),
+                   pred_error = rnorm(round(sample_size*nrow(mydata),0),
+                                      0,
+                                      sd_original_residuals),
+                   y_var = y_pop + pred_error) %>%
+            dplyr::select(x_cont, x_cat, y_var) %>%
+            mutate(y_var = unname(y_var))
+        }
+
+        simdat <-  simdat %>% map(~ lm(y_var ~ x_cont * x_cat, data = .))
+
+        tmp1 <- simdat %>%
+          map_df(broom::tidy, .id = "id") %>%
+          filter(grepl(":", term)) %>%
+          dplyr::select(replicate = id,
+                        slope_difference = estimate,
+                        p_value = p.value) %>%
+          mutate(replicate = as.integer(replicate))%>%
+          na.omit()
+
+        tmp1 <- simdat %>%
+          map_df(broom::glance, .id = "id") %>%
+          select(r2_adj = adj.r.squared) %>%
+          bind_cols(tmp1,.)%>%
+          na.omit()
+
+        } else if(type == "null-hypothesis"){
+
+        simdat = list()
+
+        for(i in 1:reps){
+          # sampling data (predicted values + sampling error)
+          simdat[[i]] <-
+            data.frame(x_cont = sample(dens_distr_x[[1]],
+                                       size = round(sample_size*nrow(mydata),0),
+                                       prob = dens_distr_x[[2]],
+                                       replace=F),
+                       x_cat = sample(cat_var$x_cat,
+                                      size = round(sample_size*nrow(mydata),0),
+                                      prob = cat_var$nr,
+                                      replace = T)) %>%
+            mutate(y_var = rnorm(round(sample_size*nrow(mydata),0),
+                                 0,
+                                 sd_original_residuals)) %>%
+            mutate(y_var = unname(y_var))
+        }
+
+        simdat <-  simdat %>% map(~ lm(y_var ~ x_cont * x_cat, data = .))
+
+        tmp1 <- simdat %>%
+          map_df(broom::tidy, .id = "id") %>%
+          filter(grepl(":", term)) %>%
+          dplyr::select(replicate = id,
+                        slope_difference = estimate,
+                        p_value = p.value) %>%
+          mutate(replicate = as.integer(replicate))%>%
+          na.omit()
+
+        tmp1 <- simdat %>%
+          map_df(broom::glance, .id = "id") %>%
+          select(r2_adj = adj.r.squared) %>%
+          bind_cols(tmp1,.)%>%
+          na.omit()
+
+      } else {
+        stop("set type at either 'null-hypothesis', or 'confidence-interval'")
+      }
+
+    } else if(equal_variance == FALSE){
+
+        # getting regression statistics of original data
+        # Linear regression model
+        regr_model <- lm(y_var ~ x_cont * x_cat, data = mydata)
+
+        # Density distribution of the continuous x-values
+        dens_distr_x <-density(mydata$x_cont,
+                               n=max(1024,
+                                     round(sample_size*nrow(mydata),0)))
+
+        # Levels of the categorical x-value, with original proportions
+        suppressMessages(
+          cat_var <- mydata %>% dplyr::group_by(x_cat) %>% dplyr::summarise(nr = n())
+          )
+
+        # get standard deviation of the residuals of original regression model
+        # But calculated separately for each of the two groups
+        suppressMessages(
+          sd_original_residuals <- mydata %>%
+          mutate(predy = predict(regr_model)) %>%
+          group_by(x_cat) %>%
+          dplyr::summarise(residuals = error_term*sd(predy-y_var))
+          )
+
+
+        if(type == "confidence-interval"){
+
+          simdat = list()
+
+          for(i in 1:reps){
+            # sampling data (predicted values + sampling error)
+            simdat[[i]] <-
+              data.frame(x_cont = sample(dens_distr_x[[1]],
+                                         round(sample_size*nrow(mydata),0),
+                                         prob = dens_distr_x[[2]],
+                                         replace=F),
+                         x_cat = sample(cat_var$x_cat,
+                                        size = round(sample_size*nrow(mydata),0),
+                                        prob = cat_var$nr,
+                                        replace = T)) %>%
+              left_join(sd_original_residuals, by = "x_cat") %>%
+              rowwise() %>%
+              mutate(res   = error_term*rnorm(1, mean=0, sd=residuals)) %>%
+              ungroup() %>%
+              mutate(y_pop = predict(regr_model, newdata = .)) %>%
+              mutate(y_var = y_pop + res) %>%
+              #dplyr::select(x_cont, x_cat, y_var) %>%
+              mutate(y_var = unname(y_var))
+          }
+
+          simdat <-  simdat %>% map(~ lm(y_var ~ x_cont * x_cat, data = .))
+
+          tmp1 <- simdat %>%
+            map_df(broom::tidy, .id = "id") %>%
+            filter(grepl(":", term)) %>%
+            dplyr::select(replicate = id,
+                          slope_difference = estimate,
+                          p_value = p.value) %>%
+            mutate(replicate = as.integer(replicate))%>%
+            na.omit()
+
+          tmp1 <- simdat %>%
+            map_df(broom::glance, .id = "id") %>%
+            select(r2_adj = adj.r.squared) %>%
+            bind_cols(tmp1,.)%>%
+            na.omit()
+
+        } else if(type == "null-hypothesis"){
+
+          simdat = list()
+
+          for(i in 1:reps){
+            # sampling data (predicted values + sampling error)
+            simdat[[i]] <-
+              data.frame(x_cont = sample(dens_distr_x[[1]],
+                                         size = round(sample_size*nrow(mydata),0),
+                                         prob = dens_distr_x[[2]],
+                                         replace=F),
+                         x_cat = sample(cat_var$x_cat,
+                                        size = round(sample_size*nrow(mydata),0),
+                                        prob = cat_var$nr,
+                                        replace = T)) %>%
+              left_join(sd_original_residuals, by = "x_cat") %>%
+              rowwise() %>%
+              mutate(y_var = rnorm(1, 0, residuals)) %>%
+              ungroup() %>%
+              mutate(y_var = unname(y_var))
+          }
+
+          simdat <-  simdat %>% map(~ lm(y_var ~ x_cont * x_cat, data = .))
+
+          tmp1 <- simdat %>%
+            map_df(broom::tidy, .id = "id") %>%
+            filter(grepl(":", term)) %>%
+            dplyr::select(replicate = id,
+                          slope_difference = estimate,
+                          p_value = p.value) %>%
+            mutate(replicate = as.integer(replicate))%>%
+            na.omit()
+
+          tmp1 <- simdat %>%
+            map_df(broom::glance, .id = "id") %>%
+            select(r2_adj = adj.r.squared) %>%
+            bind_cols(tmp1,.)%>%
+            na.omit()
+
+          } else {
+          stop("set type at either 'null-hypothesis', or 'confidence-interval'")
+        }
+    }
+
+    attr(tmp1, "simulation_settings") <- list(continuous_variable = continuous_variable,
+                                              categorical_variable = categorical_variable,
+                                              response_variable = response_variable,
+                                              type = type,
+                                              reps = reps,
+                                              sample_size = sample_size,
+                                              error_term = error_term,
+                                              equal_variance = equal_variance)
+
+    attr(tmp1, "original_regr_model") <- list(tidy_output = broom::tidy(regr_model),
+                                              glance_output = broom::glance(regr_model))
+
+    attr(tmp1, "stat_value_for_comparison") <- broom::tidy(regr_model)[4,2]
+
+    attr(tmp1, "statistics_to_be_tested") <- "difference in regression slopes"
+
+    return(tmp1)
+}
+
+
+#devtools::install_github("datadrivenyale/climactor", build_vignettes = TRUE)
+
+
+
+
+
+
+
+
+
+
---
+++ R/specify_simulation.R
@@ -0,0 +1,297 @@
+#' defines all the settings for your analysis and formats your data accordingly.
+#' Currently, only two predictor variables are allowed in the case of a regression
+#' analysis, and maximum one of the predictor variables can be a categorical
+#' variable. The categorical variable can only have two levels.
+#' @df dataframe with input data.
+#' @test type of test. Options: regression slope, difference between two regression
+#' slopes. More to come.
+#' @procedure choose whether you want to test a null-hypothesis (of a mean, regression
+#' slope, slope difference or interaction effect of zero) or get a confidence
+#' interval around the observed sample statistic.
+#' @heterosc_cont
+#' @cont_var_1 Assign a value to 'error_cont_1' or 'error_cont_1', depending whether
+#' you used cont_var_1 or cont_var_2 to assign the continuous variable. A value
+#' of 1 means that the error term is computed as the sd of the residuals of the
+#' regression model on the original data. Values of 0.5 and 2 mean that the error
+#' term is half or twice that, respectively, etc. However, if you set the argument
+#' 'heteroscedasticity' == TRUE, the error term will be calculated as function
+#' of the continuous predictor variable. Use this argument when only one continuous
+#' variable is used!
+#' @cont_var_2 idem. A second continuous variable is only used when you want to
+#' explore interaction effects.Not yet implemented!
+#' @cat_var Categorical variable. All test options except for 'Chi-square' only
+#' allow for two levels within the categorical variable.
+#' @resp_var Numerical response variable
+#' @error_cont_1 Multiplies the sd of the residuals of the regression model fitted
+#' on the original data with given number. A value of '1' (default) means that
+#' the error term of the model is the same as in the original data.
+#' @heterosc_cont_1 A single number that defines how much smaller/bigger the
+#' error term is at the highest value of the observed range of predictor values
+#' than at the lowest value of the observed range of predictor values. The error
+#' terms are then calculated such that the average of the error term over the
+#' whole range of predictor values equals 'error_cont_1' * the sd of the residuals
+#' of the regression model fitted on the original data.
+#' @error_cont_2 see 'error_cont_1'
+#' @heterosc_cont_1 see 'heterosc_cont_1'
+#' @error_cat Option (1): Assign a vector with named values, one for each of the
+#' levels of the categorical variable. A value of 1 means that the error
+#' term is computed as the sd of the residuals of the regression model on the
+#' original data. Values of 0.5 and 2 mean that the error term is half or twice
+#' that, respectively, etc. Option (2): "as_data". This results in the calculation
+#' of separate error terms - based on the original data - per level of the
+#' categorical variable.
+#' residuals.
+#' @import readr
+#' @import purrr
+#' @import tidyr
+#' @import tidyselect
+#' @import dplyr
+#' @export
+specify_simulation <- function(
+  df               = forest_succession,
+  test             = "difference between regression slopes",
+  procedure        = "confidence interval",
+  cont_var_1       = "age",
+  cont_var_2       = NULL,
+  cat_var          = "top_pos",
+  resp_var         = "sr",
+  error_cat        = "as_data", #c(LS=1, US=1),
+  error_cont_1     = 1,
+  heterosc_cont_1  = 1,
+  error_cont_2     = 1,
+  heterosc_cont_2  = 1){
+
+#---difference between regression slopes----------------------------------------
+  if(test == "difference between regression slopes"){
+
+    if(is.null(cat_var)){
+      stop("Testing for the difference between two regression slopes requires
+           one continuous and one categorical variable")
+    }
+
+    if(!is.null(cont_var_1) & !is.null(cont_var_2)){
+      stop("This option requires one categorical and (only) one continuous
+      variable. Use the argument 'cont_var_1' to define the latter")
+    }
+
+    if(is.null(cont_var_1) & !is.null(cont_var_2)){
+      stop("Use the argument 'cont_var_1' to define your (only) continuous variable")
+    }
+
+    if(is.null(cat_var) & is.null(cont_var_1) & is.null(cont_var_2)){
+      stop("Testing for the difference between two regression slopes requires
+           one continuous and one categorical variable")
+    }
+
+    if(is.null(resp_var)){
+      stop("You need a response variable")
+    }
+
+    nr_levels <- df %>% distinct(.[all_of(cat_var)]) %>% count() # FIGURE OUT HOW TO USE EXTRACTED VARIABLE AS COLUMN NAME IN DISTINCT()
+    if(nr_levels != 2){
+       stop("Currently the option 'difference between regression slopes' only
+            supports two levels for the categorical variable")
+    }
+
+    if(!is.numeric(df[,all_of(cont_var_1)]) & !is.numeric(df[,all_of(resp_var)])){
+      stop("Both your response and predictor variables are not numeric")
+    }
+
+    if(!is.numeric(df[,all_of(cont_var_1)])){
+      stop("your predictor variable is not numeric")
+    }
+
+    if(!is.numeric(df[,all_of(resp_var)])){
+      stop("your response variable is not numeric")
+    }
+
+
+    if(length(error_cat) != 2 & error_cat[1] != "as_data"){
+      stop("You have two options for this argument. (1) Assign a vector with two
+      named values, one for each of the levels of the categorical variable. A
+      value of 1 means that the error term is computed as the sd of the residuals
+      of the regression model on the original data. Values of 0.5 and 2 mean that
+      the error term is half or twice that, respectively, etc. Assigning two
+      times the same value thus means that the error term of both levels are
+      calculated as the overall error term of the original data. (2) 'as_data'.
+      This option results in the calculation of seperate error terms per level
+      of the categorical variable - based on the original data.")
+    }
+
+    df <- df %>%
+          dplyr::select(tidyselect::all_of(cont_var_1),
+                        tidyselect::all_of(cat_var),
+                        tidyselect::all_of(resp_var))
+
+    # Adding attributes to data frame
+    test_procedure <- list(
+      attr(df, "from")      <- "specify_simulation",
+      attr(df, "test")      <- test,
+      attr(df, "procedure") <- procedure
+    )
+
+    variables <- list(
+      attr(df, "response_variable")    <- resp_var,
+      attr(df, "continuous_predictor")  <- cont_var_1,
+      attr(df, "categorical_predictor") <- cat_var
+    )
+
+    if(length(error_cat) == 2){
+      error_terms <- list(
+        attr(df, "error_cat")       <- data.frame(error_cat) %>%
+        rownames_to_column(all_of("top_pos")),
+        attr(df, "error_cont_1")    <- error_cont_1,
+        attr(df, "heterosc_cont_1") <- heterosc_cont_1)
+    } else {
+      error_terms <- list(
+        attr(df, "error_cat")       <- "as_data",
+        attr(df, "error_cont_1")    <- error_cont_1,
+        attr(df, "heterosc_cont_1") <- heterosc_cont_1)
+      }
+
+    return(df)
+
+# ---regression slope ----------------------------------------------------------
+  } else if(test == "regression slope"){
+
+    if(!is.null(cat_var)){
+      stop("The option to test the regression slope requires only
+           one continuous predictor and no categorical variable.
+           Use the argument 'cont_var_1' to define the former")
+    }
+
+    if(!is.null(cont_var_1) & !is.null(cont_var_2)){
+      stop("This option requires only one continuous predictor
+      variable. Use the argument 'cont_var_1' to define this predictor variable")
+    }
+
+    if(is.null(cont_var_1) & !is.null(cont_var_2)){
+      stop("Use the argument 'cont_var_1' to define your (only) continuous variable")
+    }
+
+    if(is.null(cat_var) & is.null(cont_var_1) & is.null(cont_var_2)){
+      stop("The option to test the regression slope requires one continuous
+           predictor. Use the argument 'cont_var_1' to define the former")
+    }
+
+    if(is.null(resp_var)){
+      stop("You need a response variable")
+    }
+
+
+    if(!is.numeric(df[,all_of(cont_var_1)]) & !is.numeric(df[,all_of(resp_var)])){
+      stop("Both your response and predictor variables are not numeric")
+    }
+
+    if(!is.numeric(df[,all_of(cont_var_1)])){
+      stop("your predictor variable is not numeric")
+    }
+
+    if(!is.numeric(df[,all_of(resp_var)])){
+      stop("your response variable is not numeric")
+    }
+
+
+    df <- df %>%
+      dplyr::select(tidyselect::all_of(cont_var_1),
+                    tidyselect::all_of(resp_var))
+
+    # Adding attributes to data frame
+    test_procedure <- list(
+      attr(df, "from")      <- "specify_simulation",
+      attr(df, "test")      <- test,
+      attr(df, "procedure") <- procedure
+    )
+
+    variables <- list(
+      attr(df, "response_variable")     <- resp_var,
+      attr(df, "predictor_variable")  <- cont_var_1
+    )
+
+    error_terms <- list(
+      attr(df, "error_cont_1")    <- error_cont_1,
+      attr(df, "heterosc_cont_1") <- heterosc_cont_1
+    )
+
+    return(df)
+
+#---difference between sample means---------------------------------------------
+  } else if(test == "difference between sample means"){
+
+    if(is.null(cat_var)){
+      stop("Testing for the difference between two sample means requires
+           one categorical variable")
+    }
+
+    if(!is.null(cont_var_1) | !is.null(cont_var_2)){
+      stop("This option requires only one categorical and no continuous
+      predictor variable(s). Use the argument 'cat_var' to define the former")
+    }
+
+    if(is.null(resp_var)){
+      stop("You need a response variable")
+    }
+
+    nr_levels <- df %>% distinct(.[all_of(cat_var)]) %>% count()
+    if(nr_levels != 2){
+      stop("The option 'difference between sample means' only supports two levels
+      for the categorical variable. Consider if the Chi-square test is an option")
+    }
+
+    if(!is.numeric(df[,all_of(resp_var)])){
+      stop("your response variable is not numeric")
+    }
+
+    if(length(error_cat) != 2 | error_cat != "as_data"){
+      stop("You have two options for this argument. (1) Assign a vector with two
+      named values, one for each of the levels of the categorical variable. A
+      value of 1 means that the error term is computed as the sd of the residuals
+      of the regression model on the original data. Values of 0.5 and 2 mean that
+      the error term is half or twice that, respectively, etc. Assigning two
+      times the same value thus means that the error term of both levels are
+      calculated as the overall error term of the original data. (2) 'as_data'.
+      This option results in the calculation of seperate error terms per level
+      of the categorical variable - based on the original data.")
+    }
+
+    df <- df %>%
+      dplyr::select(tidyselect::all_of(cat_var),
+                    tidyselect::all_of(resp_var))
+
+    # Adding attributes to data frame
+    test_procedure <- list(
+      attr(df, "from")      <- "specify_simulation",
+      attr(df, "test")      <- test,
+      attr(df, "procedure") <- procedure
+    )
+
+    variables <- list(
+      attr(df, "response_variable")    <- resp_var,
+      attr(df, "predictor_variable")   <- cat_var
+    )
+
+    if(error_cat == "as_data"){
+      error_terms <- list(
+        attr(df, "error_cat") <- "as_data")
+    } else {
+      error_terms <- list(
+        attr(df, "error_cat") <- data.frame(error_cat))
+    }
+
+    return(df)
+
+#---opton does not exist--------------------------------------------------------
+
+  } else { stop("at this moment the only option are 'difference between sample
+                means', regression slope' and 'difference between regression
+                slopes'")}
+
+}
+
+
+
+
+
+
+
+
